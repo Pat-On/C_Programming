96e7cb906ea1df37e8d9bcdd5fa58a82ad19c3bb
Patryk <patryk.r.nowak@gmail.com>
2026-03-01 08:48:33 +0000

    min max

---
+++ C_Programming_Bootcamp-The_Complete_C_Language_Course/Section_22_Pointers/max_min.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void findMinMax(int num1, int num2, int *pMax, int *pMin)
+{
+    if (num1 > num2)
+    {
+        *pMin = num2;
+        *pMax = num1;
+    }
+    else
+    {
+        *pMin = num1;
+        *pMax = num2;
+    }
+    // No return needed (void function)
+}
+
+int main()
+{
+    int a = 5, b = 7;
+    int max, min;
+    
+    findMinMax(a, b, &max, &min);
+    
+    printf("max between %d and %d = %d\n", a, b, max);
+    printf("min between %d and %d = %d\n", a, b, min);
+    
+    return 0;
+}
