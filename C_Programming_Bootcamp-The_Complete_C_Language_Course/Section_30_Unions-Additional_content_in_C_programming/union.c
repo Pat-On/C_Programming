fa17755dbb0d0dc6c2dfd8d2944cd81d9c707b52
Patryk <patryk.r.nowak@gmail.com>
2026-03-05 07:45:13 +0000

    union 2

---
+++ C_Programming_Bootcamp-The_Complete_C_Language_Course/Section_30_Unions-Additional_content_in_C_programming/union.c
@@ -0,0 +1,27 @@
+// how to define union
+
+union info{
+    char firstName[20];
+    int age;
+};
+
+
+union minmax{
+    int min;
+    int max;
+
+};
+
+
+int main()
+{
+    union info myVariable1;
+
+    
+
+
+
+
+
+    return 0;
+}
