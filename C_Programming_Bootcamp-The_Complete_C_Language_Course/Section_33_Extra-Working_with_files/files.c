a472364f98e70e0f618f568f4da09e77c3599517
Patryk <patryk.r.nowak@gmail.com>
2026-03-09 07:27:57 +0000

    files c

--- C_Programming_Bootcamp-The_Complete_C_Language_Course/Section_33_Extra-Working_with_files/files.c
+++ C_Programming_Bootcamp-The_Complete_C_Language_Course/Section_33_Extra-Working_with_files/files.c
@@ -5,5 +5,25 @@
 
 int main()
 {
+    // Pointer to the file
+
+    // STEP 1
+    FILE* fp;       // creating a pointer that points to a FILE type
+
+    // STEP 2 - opening / access to a file
+    // fp = fopen(<file_name>, <type_of_operation>);
+    // Types:
+    // 'w' writing 'r' - reading 'a' - appending
+    fp = fopen();
+
+    // if (fp != NULL) - to make sura that the open operation was successful
+
+    // step 4 - work with a file
+    // code for working with the file...
+
+
+    // step 5 - close the file
+    fclose(fp);
+
     return 0;
 }
