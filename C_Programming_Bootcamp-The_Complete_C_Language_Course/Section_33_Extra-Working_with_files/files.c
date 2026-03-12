2d7ec669f7fba292f3d05be0f0a88a9f248915a8
Patryk <patryk.r.nowak@gmail.com>
2026-03-12 09:01:29 +0000

    fgetc

--- C_Programming_Bootcamp-The_Complete_C_Language_Course/Section_33_Extra-Working_with_files/files.c
+++ C_Programming_Bootcamp-The_Complete_C_Language_Course/Section_33_Extra-Working_with_files/files.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 
 
+
+// fgetc - file get character
+
 int main()
 {
     // Pointer to the file
@@ -10,23 +13,34 @@ int main()
     // STEP 1
     FILE* fp;       // creating a pointer that points to a FILE type
 
+    char myChar1;
     // STEP 2 - opening / access to a file
     // fp = fopen(<file_name>, <type_of_operation>);
     // Types:
     // 'w' writing 'r' - reading 'a' - appending
-    fp = fopen("myFile.txt", "w"); // "w" - create file or overwrite file if exists
+    fp = fopen("myFile.txt", "r"); // "w" - create file or overwrite file if exists
 
     // if (fp != NULL) - to make sura that the open operation was successful
     if (fp == NULL)
         printf("Failed");
     else 
     {
-        printf("Failed is open");
+        printf("file is open \n");
     
+        myChar1 = fgetc(fp);
+        printf("%c \n",myChar1);
+
+        myChar1 = fgetc(fp);
+        printf("%c \n",myChar1);
         // step 4 - work with a file
         // code for working with the file...
 
+
+        // specifying stdin
+        fgetc(stdin); // you read one character from the keyboard - fgetc is quite flexible
+
         // step 5 - close the file
+
         fclose(fp);
     }
 
