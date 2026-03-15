3b679749eb344889ec8c2e326c6dbe9d97a7ebb5
Patryk <patryk.r.nowak@gmail.com>
2026-03-15 08:36:09 +0000

    example:

---
+++ C_Programming_Masterclass-Pointers_and_Advanced_C_Language/Section_2_Pointers-Introduction_to_basics/12_Exclusive_pass_by_reference_guide.md
@@ -0,0 +1,9 @@
+# Exclusive pass by reference guide
+
+## single return - you can not return two values
+![alt text](image-7.png)
+
+## Solution - Pointers
+- one function - both variables
+- to modify by reference - sending addresses to the function and the function that received addresses is going to modify them
+
---
+++ C_Programming_Masterclass-Pointers_and_Advanced_C_Language/Section_2_Pointers-Introduction_to_basics/12_code.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void findMinMax(int num1, int num2, int *pMax, int *pMin)
+{
+    if (num1 > num2)
+    {
+        *pMin = num2;
+        *pMax = num1;
+    }
+    else
+    {
+        *pMin = num1;
+        *pMax = num2;
+    }
+    // No need for any return
+}
+
+int main()
+{
+    int a = 3, b = 9, max, min;
+
+    // printf("Enter two numbers: ");
+    // scanf("%d %d", &a, &b);
+
+    findMinMax(a, b, &max, &min);
+
+    printf("Max = %d, Min = %d\n", max, min);
+
+    return 0;
+}
