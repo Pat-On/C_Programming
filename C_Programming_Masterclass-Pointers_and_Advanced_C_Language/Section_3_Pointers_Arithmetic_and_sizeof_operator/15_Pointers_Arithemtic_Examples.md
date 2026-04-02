e511e61bc3f0be21cd392c0d31f89d8189bf3f10
Patryk <patryk.r.nowak@gmail.com>
2026-04-02 07:36:08 +0100

    nice

--- C_Programming_Masterclass-Pointers_and_Advanced_C_Language/Section_3_Pointers_Arithmetic_and_sizeof_operator/15_Pointers_Arithemtic_Examples.md
+++ C_Programming_Masterclass-Pointers_and_Advanced_C_Language/Section_3_Pointers_Arithmetic_and_sizeof_operator/15_Pointers_Arithemtic_Examples.md
@@ -1,1 +1,3 @@
-# Pointers Arithmetic Exampl
+# Pointers Arithmetic Examples
+
+![alt text](image-2.png)
---
+++ C_Programming_Masterclass-Pointers_and_Advanced_C_Language/Section_3_Pointers_Arithmetic_and_sizeof_operator/15_code.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main()
+{
+    int grades[3] = {80, 90, 100};
+
+    int *gradesPtr = grades;
+
+    printf("\n grades start at: %p \n", gradesPtr);
+    printf("\n grades start at: %p \n", gradesPtr+1);
+
+    printf("Pointer address as decimal: %llu\n", (unsigned long long)(uintptr_t)gradesPtr);
+    printf("Pointer address as decimal: %llu\n", (unsigned long long)(uintptr_t)1+gradesPtr);
+
+    printf("Pointer address as decimal: %llu\n", (unsigned long long)(uintptr_t)(gradesPtr+1));
+
+
+
+}
