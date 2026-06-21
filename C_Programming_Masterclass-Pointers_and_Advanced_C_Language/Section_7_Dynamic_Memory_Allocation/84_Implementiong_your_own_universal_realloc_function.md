376268314be9706455ca472520e4cd2e5b162b32
Patryk <patryk.r.nowak@gmail.com>
2026-06-21 19:25:07 +0100

    task - create your own realloc

---
+++ C_Programming_Masterclass-Pointers_and_Advanced_C_Language/Section_7_Dynamic_Memory_Allocation/84_Implementiong_your_own_universal_realloc_function.md
@@ -0,0 +1,9 @@
+# Exercise to finish.
+
+
+- Where is the catch?
+
+- General - Universal - it mean that it should work universally across a different types
+  - Basically general function
+  - We need to mimic the original Realloc
+
---
+++ C_Programming_Masterclass-Pointers_and_Advanced_C_Language/Section_7_Dynamic_Memory_Allocation/84_maloc.c
@@ -0,0 +1,11 @@
+// Develop your special "realloc" function.
+// The function should have the following signature:
+
+// void* myRealloc(void *srcblock, unsigned oldsize, unsigned newsize);
+
+// The function should implement a GENERAL/UNIVERSAL reallocation functionality
+// (without using the realloc function...)
+int main()
+{
+
+}  
