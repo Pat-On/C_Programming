a683ac266c60dfe7aea5447e33983c0103a36bc6
Patryk <patryk.r.nowak@gmail.com>
2026-06-22 20:09:55 +0100

    half way

--- C_Programming_Masterclass-Pointers_and_Advanced_C_Language/Section_7_Dynamic_Memory_Allocation/84_maloc.c
+++ C_Programming_Masterclass-Pointers_and_Advanced_C_Language/Section_7_Dynamic_Memory_Allocation/84_maloc.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 // Develop your special "realloc" function.
 // The function should have the following signature:
 
@@ -5,7 +7,25 @@
 
 // The function should implement a GENERAL/UNIVERSAL reallocation functionality
 // (without using the realloc function...)
+void* myRealloc(void* srcblock, unsigned oldsize, unsigned newsize);
+
 int main()
 {
+	int* numbers = (int*)malloc(3 * sizeof(int));
+	int* newNumbers = NULL;
+	if (!numbers) // numbers == NULL
+		return 1;
+	numbers[0] = 3;
+	numbers[1] = 5;
+	numbers[2] = 4;
+	newNumbers = (int*)myRealloc(numbers, 3 * sizeof(int), 4 * sizeof(int));
+}
 
-}  
+void* myRealloc(void* srcblock, unsigned oldsize, unsigned newsize)
+{
+    int* resultArr = (int*)malloc(newsize);
+
+    //      srcblock is a void* — a pointer with no type information. The compiler doesn't know the size of what it points to, so
+    //   srcblock[0] is meaningless: it can't calculate how many bytes to read.
+    resultArr[0] = srcblock[0];
+}
