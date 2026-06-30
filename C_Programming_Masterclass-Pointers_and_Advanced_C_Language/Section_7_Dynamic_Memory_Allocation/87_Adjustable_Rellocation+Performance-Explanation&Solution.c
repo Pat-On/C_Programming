8ce525adc4c3fb35a1a8d387dd2a608b3fa7557e
Patryk <patryk.r.nowak@gmail.com>
2026-06-30 19:30:57 +0100

    87 task

---
+++ C_Programming_Masterclass-Pointers_and_Advanced_C_Language/Section_7_Dynamic_Memory_Allocation/87_Adjustable_Rellocation+Performance-Explanation&Solution.c
@@ -0,0 +1,4 @@
+// TASK: Write a function that creates and fills with values a dynamic array.
+//       The size of the array is UNKNOWN in advance.
+//       What's known is that once we read a value of "-1", the function should stop
+//       reading inputs from the user, and it should return the array.
